41bb83ff80741a1f20cd63d5d0f7eff4166e206f
amishaanyk <amishanyk02@gmail.com>
2024-07-01 23:43:06 +0530

    Update footer.js

--- tutorial-react/src/components/footer.js
+++ tutorial-react/src/components/footer.js
@@ -17,9 +17,9 @@ export const Footer = () => {
                     </Col>
                     <Col sm={6} className="text-center text-sm-end">
                         <div className="social-icon">
-                            <a href="https://www.linkedin.com/in/james-plamondon-369359bb/"><img src={navIcon1} alt="LinkedIn" /></a>
-                            <a href="https://www.facebook.com/james.plamondon74/"><img src={navIcon2} alt="Facebook" /></a>
-                            <a href="#"><img src={navIcon3} alt="Instagram" /></a>
+                            <a href="https://www.linkedin.com/in/james-plamondon-369359bb/" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="LinkedIn" /></a>
+                            <a href="https://www.facebook.com/james.plamondon74/" target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt="Facebook" /></a>
+                            <a href="#" target="_blank" rel="noopener noreferrer"><img src={navIcon3} alt="Instagram" /></a>
                         </div>
                         <p>Copyright 2024. All Rights Reserved.</p>
                     </Col>
