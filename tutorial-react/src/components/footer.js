63e4f495fa192943b380d8306d832dc67a86f644
amishaanyk <amishanyk02@gmail.com>
2024-07-01 23:15:16 +0530

    Update footer.js

--- tutorial-react/src/components/footer.js
+++ tutorial-react/src/components/footer.js
@@ -1,5 +1,6 @@
-import { Col, Container, Row } from "react-bootstrap"
-import { SkillsShowcase } from './newsletter'
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { SkillsShowcase } from './newsletter';
 import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
@@ -9,18 +10,18 @@ export const Footer = () => {
     return (
         <footer className="footer">
             <Container>
-                <Row className="align-item-center">
+                <Row className="align-items-center">
                     <SkillsShowcase />
                     <Col sm={6}>
-                        <img src={logo} alt='logo' />
+                        <img src={logo} alt="Logo" />
                     </Col>
                     <Col sm={6} className="text-center text-sm-end">
                         <div className="social-icon">
-                            <a href="https://www.linkedin.com/in/james-plamondon-369359bb/"><img src={navIcon1} /></a>
-                            <a href="https://www.facebook.com/james.plamondon74/"><img src={navIcon2} /></a>
-                            <a href=""><img src={navIcon3} /></a>
+                            <a href="https://www.linkedin.com/in/james-plamondon-369359bb/"><img src={navIcon1} alt="LinkedIn" /></a>
+                            <a href="https://www.facebook.com/james.plamondon74/"><img src={navIcon2} alt="Facebook" /></a>
+                            <a href="#"><img src={navIcon3} alt="Instagram" /></a>
                         </div>
-                        <p>Copyright 2024. All Right Reserved.</p>
+                        <p>Copyright 2024. All Rights Reserved.</p>
                     </Col>
                 </Row>
             </Container>
